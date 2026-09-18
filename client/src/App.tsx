601d90a54f4b3c9bcc0918032cf056685037782a
Parth Jain <pj3918127@gmail.com>
2026-09-18 12:53:24 +0530

    added session guard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,40 +4,41 @@ import { AppProvider } from "./contexts/AppContext";
 import Toast from "./components/ui/Toast";
 import AppShell from "./components/AppShell";
 import LoginPage from "./views/LoginPage";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 function SessionGuard() {
   const { signOut } = useClerk();
-  const [message, setMessage] = useState<string | null>(null);
 
   useEffect(() => {
     const handler = async () => {
-      setMessage("Your session has expired. Please sign in again.");
-      try { await signOut(); } catch {}
+      try {
+        await signOut();
+      } catch {}
     };
+
     window.addEventListener("ats:session-expired", handler);
-    return () => window.removeEventListener("ats:session-expired", handler);
+
+    return () => {
+      window.removeEventListener("ats:session-expired", handler);
+    };
   }, [signOut]);
 
-  return message ? <LoginPage /> : <SessionGuard />;
+  return <AppShell />;
 }
 
 function App() {
   return (
     <ToastProvider>
       <AppProvider>
-        
         <SignedIn>
-          <AppShell />
+          <SessionGuard />
         </SignedIn>
 
         <SignedOut>
           <LoginPage />
         </SignedOut>
-
       </AppProvider>
-      
-      {/* Toast notifications render globally */}
+
       <Toast />
     </ToastProvider>
   );
